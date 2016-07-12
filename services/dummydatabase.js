f7d6d430f5d146d5cfca73c4e3908ddeaf4f9a82
Emmett Walsh <emmettwalsh@gmail.com>
2016-07-12 16:03:11 -0400

    fix

--- services/dummydatabase.js
+++ services/dummydatabase.js
@@ -1,59 +1,62 @@
 
 
 
-exports.db = {
-    restaurant : [
-        {
-            productId: 1,
-            name: "5 Napkin Burger",
-            city: "New York",
-            url: "http://5napkinburger.com/",
-            image: "http://bot-mediator.herokuapp.com/UWS/Logo_Restaurants/5%20Napkin%20Burger/5%20Napkin%20Logo.jpg",
-            coupon: "http://bot-mediator.herokuapp.com/UWS/Logo_Restaurants/QR_Code_Coupon/images.png"
-        },
-        {
-            productId: 2,
-            name: "PJ Clarke's",
-            city: "New York",
-            url: "http://pjclarkes.com/",
-            image: "http://www.crainsnewyork.com/apps/pbcsi.dll/storyimage/CN/20100110/SMALLBIZ/301109968/AR/0/P.J.-Clarke's&imageversion=widescreen&maxw=770",
-            coupon: "http://bot-mediator.herokuapp.com/UWS/Logo_Restaurants/QR_Code_Coupon/images.png"
-        },
-        {
-            productId: 3,
-            name: "McDonalds",
-            city: "Boston",
-            url: "http://www.mcdonalds.com/us/en/home.html",
-            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Mcdonalds-90s-logo.svg/2000px-Mcdonalds-90s-logo.svg.png",
-            coupon: "http://bot-mediator.herokuapp.com/UWS/Logo_Restaurants/QR_Code_Coupon/images.png"
-        }
-    ],
+exports.db = function() {
 
-    clothing : [
-        {
-            productId: 4,
-            name: "The Gap",
-            city: "New York",
-            url: "http://www.gap.com/",
-            image: "https://lh6.ggpht.com/LKRb7hffPEcZOvKWHUpGo-7ajEYkcMXQw8ewHldpydXfl0hG2K4Ae35NxffRmUU4LZmM=w300",
-            coupon: "http://bot-mediator.herokuapp.com/UWS/Logo_Restaurants/QR_Code_Coupon/images.png"
-        },
-        {
-            productId: 5,
-            name: "Banana Republic",
-            city: "New York",
-            url: "http://bananarepublic.com/",
-            image: "http://images.military.com/media/mail/deals-and-discounts/bananarepublic.jpg",
-            coupon: "http://bot-mediator.herokuapp.com/UWS/Logo_Restaurants/QR_Code_Coupon/images.png"
-        },
-        {
-            productId: 6,
-            name: "Old Navy",
-            city: "Boston",
-            url: "www.oldnavy.com",
-            image: "http://res.cloudinary.com/goodsearch/image/upload/v1439940283/hi_resolution_merchant_logos/old-navy_coupons.jpg",
-            coupon: "http://bot-mediator.herokuapp.com/UWS/Logo_Restaurants/QR_Code_Coupon/images.png"
-        }
+    return {
+        restaurant: [
+            {
+                productId: 1,
+                name: "5 Napkin Burger",
+                city: "New York",
+                url: "http://5napkinburger.com/",
+                image: "http://bot-mediator.herokuapp.com/UWS/Logo_Restaurants/5%20Napkin%20Burger/5%20Napkin%20Logo.jpg",
+                coupon: "http://bot-mediator.herokuapp.com/UWS/Logo_Restaurants/QR_Code_Coupon/images.png"
+            },
+            {
+                productId: 2,
+                name: "PJ Clarke's",
+                city: "New York",
+                url: "http://pjclarkes.com/",
+                image: "http://www.crainsnewyork.com/apps/pbcsi.dll/storyimage/CN/20100110/SMALLBIZ/301109968/AR/0/P.J.-Clarke's&imageversion=widescreen&maxw=770",
+                coupon: "http://bot-mediator.herokuapp.com/UWS/Logo_Restaurants/QR_Code_Coupon/images.png"
+            },
+            {
+                productId: 3,
+                name: "McDonalds",
+                city: "Boston",
+                url: "http://www.mcdonalds.com/us/en/home.html",
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Mcdonalds-90s-logo.svg/2000px-Mcdonalds-90s-logo.svg.png",
+                coupon: "http://bot-mediator.herokuapp.com/UWS/Logo_Restaurants/QR_Code_Coupon/images.png"
+            }
+        ],
 
-    ]
+        clothing: [
+            {
+                productId: 4,
+                name: "The Gap",
+                city: "New York",
+                url: "http://www.gap.com/",
+                image: "https://lh6.ggpht.com/LKRb7hffPEcZOvKWHUpGo-7ajEYkcMXQw8ewHldpydXfl0hG2K4Ae35NxffRmUU4LZmM=w300",
+                coupon: "http://bot-mediator.herokuapp.com/UWS/Logo_Restaurants/QR_Code_Coupon/images.png"
+            },
+            {
+                productId: 5,
+                name: "Banana Republic",
+                city: "New York",
+                url: "http://bananarepublic.com/",
+                image: "http://images.military.com/media/mail/deals-and-discounts/bananarepublic.jpg",
+                coupon: "http://bot-mediator.herokuapp.com/UWS/Logo_Restaurants/QR_Code_Coupon/images.png"
+            },
+            {
+                productId: 6,
+                name: "Old Navy",
+                city: "Boston",
+                url: "www.oldnavy.com",
+                image: "http://res.cloudinary.com/goodsearch/image/upload/v1439940283/hi_resolution_merchant_logos/old-navy_coupons.jpg",
+                coupon: "http://bot-mediator.herokuapp.com/UWS/Logo_Restaurants/QR_Code_Coupon/images.png"
+            }
+
+        ]
+    }
 }
